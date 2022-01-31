720ca9c5138d20fa83260735e540c5effe336155
Rustam <rustam2004sadulaev@mail.ru>
2022-02-01 02:43:45 +0300

    added two task(1-2)

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Привет Github');
+
+// Первая задача
+function solution(str) {
+	return str.split('').reverse().join('')
+}
+
+// Вторая задача
+function booleanToString(b) {
+	if (b === true) {
+		return 'true'
+	} else if (b === false) {
+		return 'false'
+	}
+}
+
