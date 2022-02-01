241f2f094271eb8a0d20eb2b8b6f826ed2f9e3aa
Rustam <rustam2004sadulaev@mail.ru>
2022-02-01 05:36:40 +0300

    task-9

--- script.js
+++ script.js
@@ -70,6 +70,13 @@ function getEvenNumbers(numbersArray) {
 
 
 //-----------------------задача-9
+function sumOfMinimums(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += Math.min(...arr[i])
+	}
+	return sum;
+}
 
 
 //-----------------------задача-10
