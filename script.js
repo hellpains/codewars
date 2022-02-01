1c933e28e411897c9efe2b028c72a86c8113f4ce
Rustam <rustam2004sadulaev@mail.ru>
2022-02-01 03:29:38 +0300

    task-6

--- script.js
+++ script.js
@@ -43,6 +43,15 @@ function stringToArray(string) {
 
 
 //-----------------------задача-6
+function positiveSum(arr) {
+	let sum = 0;
+	for (let elem of arr) {
+		if (elem >= 0) {
+			sum += elem;
+		}
+	}
+	return sum;
+}
 
 
 //-----------------------задача-7
