07505055503a028cbc71e1406be26f4025b0e2c6
hellpains <rustam2004sadulaev@mail.ru>
2022-06-26 15:45:01 +0300

    'task-38.2'

--- script.js
+++ script.js
@@ -401,15 +401,64 @@ class Person {
     constructor(myName) {
         this.name = myName
     }
-    greet(yourName){
+
+    greet(yourName) {
         return "Hello " + yourName + ", my name is " + this.name;
     }
 
 }
 
 
-
 //----------------------------------------------------38
+
+class Animal {
+    constructor(name, age, legs, species, status) {
+        this.name = name;
+        this.age = age;
+        this.legs = legs;
+        this.species = species;
+        this.status = status;
+    }
+
+    introduce() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+}
+
+class Shark extends Animal {
+    constructor(name, age, status) {
+        super(name, age, status);
+        this.legs = 0
+        this.species = "shark"
+        this.status=status
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, age, status) {
+        super(name, age, status);
+        this.legs = 4
+        this.species = "cat"
+        this.status = status
+    }
+    introduce() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+    }
+}
+
+
+class Dog extends Animal {
+    constructor(name, age, status, master) {
+        super(name, age, status);
+        this.master = master
+        this.legs = 4
+        this.species = "dog"
+        this.status=status
+    }
+    greetMaster () {
+        return `Hello ${this.master}`;
+    }
+}
 
 
 //----------------------------------------------------39
